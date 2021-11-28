0fbf8c9065628764da66c983c4d696bd9021d09e
Kengoum Gassam <kengoumgassam@gmail.com>
2021-11-28 15:10:56 +0100

    config 2

--- serveur.js
+++ serveur.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
 app.use(express.static(__dirname + "dist/SheinBoot"));
 
 app.get('/*', (req, res) => {
-    //res.sendFile(path.join(__dirname + "dist/SheinBoot"))
-    res.send("fghjkljfcgvhbjk")
+    res.sendFile("./dist/SheinBoot/index.html")
 })
 
 app.listen(process.env.PORT);
